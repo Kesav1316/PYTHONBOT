b5f94915af3ca772aa6761b48be2d0c74bf772d5
Kesav1316 <kesavram1316@gmail.com>
2023-07-12 14:16:23 +0530

    Edit main.py , responses.py , bot.py main.py to run the bot using TOKEN responses.py with few commands on what response the bot would give and methodogy of how it works bot.py to import and send messages

--- bot.py
+++ bot.py
@@ -1,11 +1,28 @@
 import discord
-from discord.ext import commands
+import responses
 
-client = commands.Bot(command_prefix="?" , intents = discord.Intents.all())
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
 
-@client.event
-async def on_ready():
-    print("Success: Bot is connected to discord")
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    with open("E:\\BOT\\PYTHONBOT\\token.txt") as f:
+        TOKEN = f.readline()
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    client.run(TOKEN)
 
 '''@client.command()   #To send in server
 async def ping(ctx):
@@ -17,9 +34,9 @@ async def pong(ctx):
 
 @client.command(aliases = ["hello"]) # Any string inside the list of aliases can be used after prefix
 async def any(ctx , * , question): # * - space ; question - anything can be done
-    await ctx.send("ok")'''
+    await ctx.send("ok")
 
 with open("E:\\BOT\\PYTHONBOT\\token.txt") as f:
     TOKEN = f.readline()
 
-client.run(TOKEN)
+client.run(TOKEN)'''
